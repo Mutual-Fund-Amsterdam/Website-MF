babd36606e57ac79195a435880873ae9bb1e7296
BRB0127 <b.r.berlijn27@hotmail.com>
2026-08-22 06:57:43 +0200

    Verplaats AEX-identificatie naar de hover

--- components/AexBackdrop.tsx
+++ components/AexBackdrop.tsx
@@ -133,13 +133,6 @@ export default function AexBackdrop() {
 
   return (
     <div className="aex-backdrop">
-      <div className="aex-meta">
-        <span className="aex-meta-label">
-          <strong>AEX-index</strong>
-          <small>Koersverloop dit jaar (YTD)</small>
-        </span>
-        <strong className="aex-meta-value">{numberFormat.format(data.latest)} punten</strong>
-      </div>
       <div className="aex-chart-wrap">
         <svg
           className="aex-chart"
@@ -202,6 +195,7 @@ export default function AexBackdrop() {
             }}
             aria-hidden="true"
           >
+            <span className="aex-tooltip-label">AEX-INDEX</span>
             <time>
               {tooltipDateFormat.format(new Date(activePoint.point.timestamp * 1000))}
             </time>
